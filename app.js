d21da9692979145756868cea6c0e14c232c2f71f
henrywinnerman <adepegbahenry@gmail.com>
2020-08-20 12:34:50 +0100

    TaskList built with HTML, BootStrap 4 and JavaScript.
    A responsive UI tasklist, with features of creating task, filtering tasks, checking completed tasks and deleting tasks. The tasks are also persisted to local storage, so tasks are safe when browser is refreshed.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const taskInput = document.querySelector('#task');
 const filterList = document.querySelector('#filter');
 const taskList = document.querySelector('#taskList');
 const clearBtn = document.querySelector('#clear');
-
+const complete = document.querySelector('#completeList');
 
 //Declare function for Event Listeners
 
@@ -14,6 +14,7 @@ function loadEventListeners() {
 document.addEventListener('DOMContentLoaded', getTasks);
 form.addEventListener('submit', addTask);
 taskList.addEventListener('click', removeTask);
+complete.addEventListener('click', completeTask);
 clearBtn.addEventListener('click', clearTask);
 filterList.addEventListener('keyup', filterTask);
 }
@@ -40,8 +41,16 @@ link.className = 'delete-item';
 // Add html
 link.innerHTML = '<div><i class = "fa fa-trash pl-3"></i></div>';
 link.style.float = 'right';
+link.style.color = 'red'
+//create complete link
+const link2 = document.createElement('a')
+link2.className = 'complete-item';
+link2.innerHTML = '<div><i class = "fa fa-check-circle"></i></div>'
+link2.style.float = 'right';
+link2.style.color = 'green';
 // Append link to list
 list.appendChild(link);
+list.appendChild(link2);
 // Append li to task list
 taskList.appendChild(list);
 
@@ -71,8 +80,16 @@ link.className = 'delete-item';
 // Add html
 link.innerHTML = '<div><i class = "fa fa-trash pl-3"></i></div>';
 link.style.float = 'right';
+link.style.color = 'red'
+//create complete link
+const link2 = document.createElement('a')
+link2.className = 'complete-item';
+link2.innerHTML = '<div><i class = "fa fa-check-circle"></i></div>'
+link2.style.float = 'right';
+link2.style.color = 'green';
 // Append link to list
 list.appendChild(link);
+list.appendChild(link2);
 // Append li to task list
 taskList.appendChild(list);
 
@@ -108,6 +125,26 @@ function removeTask(e){
 
       removeFromLocalStorage(e.target.parentElement.parentElement.parentElement);
     }
+  } else if(e.target.parentElement.parentElement.classList.contains('complete-item')){
+  let lists;
+  lists = e.target.parentElement.parentElement.parentElement.textContent;
+
+// Create Completed Task Lists
+  const list2 = document.createElement('li');
+  list2.className = 'list-group-item';
+  list2.appendChild(document.createTextNode(lists));
+
+// Create completed Task link
+   const completeLink = document.createElement('a');
+   completeLink.className ='completed';
+   completeLink.innerHTML ='<i class="fa fa-thumbs-up"></i>';
+   completeLink.style.color ='blue';
+   completeLink.style.float ='right';
+   
+
+   list2.appendChild(completeLink);
+   complete.appendChild(list2);
+  e.target.parentElement.parentElement.parentElement.remove();
   }
 
 }
@@ -126,6 +163,36 @@ function removeFromLocalStorage(taskItem){
     })
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
+
+// Removing completed task
+function completeTask(e){
+  if(e.target.parentElement.classList.contains('completed')){
+    e.target.parentElement.parentElement.remove();
+
+      // Remove completed task from LocalStorage
+      completeFromLocalStorage(e.target.parentElement.parentElement);
+  }
+}
+
+//Removing Completed Task From Storage
+function completeFromLocalStorage(taskItems){
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+  tasks = [];
+  }else{
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task, index){
+    if(taskItems.textContent === task){
+      tasks.splice(index, 1);
+    }
+  });
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+}
+
+
 
 function clearTask(){
   taskList.innerHTML = '';
